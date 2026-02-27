2c3b0d09cebad752c51d42d07fbdfd0bb02664de
chubbyhippo <mkmsacc@outlook.com>
2026-02-27 09:48:10 +0700

    Add PolicyEvents class to represent domain events for policies

---
+++ src/main/java/io/github/chubbyhippo/insurance/policy/PolicyEvents.java
@@ -0,0 +1,11 @@
+package io.github.chubbyhippo.insurance.policy;
+
+import java.util.UUID;
+
+public final class PolicyEvents {
+
+    private PolicyEvents() {}
+
+    public record PolicyActivated(UUID policyId) {}
+    public record PolicyCancelled(UUID policyId) {}
+}
