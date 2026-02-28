c5171b70d78701656d893d3568caf057a01f7000
chubbyhippo <mkmsacc@outlook.com>
2026-02-28 10:44:02 +0700

    Apply consistent code formatting to `PolicyRepository` class

--- src/main/java/io/github/chubbyhippo/insurance/policy/PolicyRepository.java
+++ src/main/java/io/github/chubbyhippo/insurance/policy/PolicyRepository.java
@@ -1,8 +1,6 @@
 package io.github.chubbyhippo.insurance.policy;
 
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
-
-public interface PolicyRepository extends JpaRepository<Policy, UUID> {
-}
+public interface PolicyRepository extends JpaRepository<Policy, UUID> {}
