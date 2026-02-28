2cd2cc0f8b13288c651871a655324dbcdfa38e8a
chubbyhippo <mkmsacc@outlook.com>
2026-02-28 10:43:20 +0700

    Apply consistent code formatting to `InvoiceRepository` class

--- src/main/java/io/github/chubbyhippo/insurance/billing/InvoiceRepository.java
+++ src/main/java/io/github/chubbyhippo/insurance/billing/InvoiceRepository.java
@@ -1,13 +1,12 @@
 package io.github.chubbyhippo.insurance.billing;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InvoiceRepository extends JpaRepository<Invoice, UUID> {
 
-    boolean existsByPolicyIdAndStatus(UUID policyId, InvoiceStatus status);
+  boolean existsByPolicyIdAndStatus(UUID policyId, InvoiceStatus status);
 
-    List<Invoice> findByPolicyIdAndStatus(UUID policyId, InvoiceStatus status);
+  List<Invoice> findByPolicyIdAndStatus(UUID policyId, InvoiceStatus status);
 }
