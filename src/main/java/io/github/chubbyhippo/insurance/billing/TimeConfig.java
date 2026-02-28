7aa2a96331c78673ca696913fc6c7a6fad3d5fd8
chubbyhippo <mkmsacc@outlook.com>
2026-02-28 10:44:24 +0700

    Apply consistent code formatting to `TimeConfig` class for improved readability

--- src/main/java/io/github/chubbyhippo/insurance/billing/TimeConfig.java
+++ src/main/java/io/github/chubbyhippo/insurance/billing/TimeConfig.java
@@ -1,15 +1,14 @@
 package io.github.chubbyhippo.insurance.billing;
 
+import java.time.Clock;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.time.Clock;
 
 @Configuration
 public class TimeConfig {
 
-    @Bean
-    public Clock clock() {
-        return Clock.systemUTC(); // or systemDefaultZone() if you prefer
-    }
+  @Bean
+  public Clock clock() {
+    return Clock.systemUTC(); // or systemDefaultZone() if you prefer
+  }
 }
