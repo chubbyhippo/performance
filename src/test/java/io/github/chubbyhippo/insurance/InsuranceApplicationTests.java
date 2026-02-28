d14276205cc2ac05ada0cbf5a0d7bf9e183ac330
chubbyhippo <mkmsacc@outlook.com>
2026-02-28 10:43:11 +0700

    Apply consistent code formatting to `InsuranceApplicationTests` class

--- src/test/java/io/github/chubbyhippo/insurance/InsuranceApplicationTests.java
+++ src/test/java/io/github/chubbyhippo/insurance/InsuranceApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class InsuranceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
