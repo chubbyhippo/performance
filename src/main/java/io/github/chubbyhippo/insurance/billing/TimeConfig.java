7624acdf5a9744b51b87f7f9e80f7f89a143f8c9
chubbyhippo <mkmsacc@outlook.com>
2026-03-11 12:24:02 +0700

    Fix formatting in TimeConfig.java by adjusting indentation

--- src/main/java/io/github/chubbyhippo/insurance/billing/TimeConfig.java
+++ src/main/java/io/github/chubbyhippo/insurance/billing/TimeConfig.java
@@ -1,14 +1,15 @@
 package io.github.chubbyhippo.insurance.billing;
 
-import java.time.Clock;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.Clock;
 
 @Configuration
 public class TimeConfig {
 
-  @Bean
-  public Clock clock() {
-    return Clock.systemUTC(); // or systemDefaultZone() if you prefer
-  }
+    @Bean
+    public Clock clock() {
+        return Clock.systemUTC();
+    }
 }
